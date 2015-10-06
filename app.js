d1927403d019815e55e460f13a53bc90f4b584b9
lengshuiyulangcn <li.ning@i.mbox.nagoya-u.ac.jp>
2015-10-06 21:13:43 +0900

    almost done

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var app = express()
 var Health=require("model/health");
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
-
 //config
 mongoose.connect('mongodb://127.0.0.1:27017') //default port
 
 //middleware
+app.set('view engine', 'ejs'); 
 app.use(express.static(__dirname + '/public'))
 app.use(partials())
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -22,24 +22,34 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
+var User = ["david","dongQiang"];
 //reset health before game start
 app.get('/admin/reset', function(req,res){
   Health.remove({}, function(err,model){
     Health.create({team: 'white', blood: 100},function(err,result){
       Health.create({team: 'red', blood: 100},function(err,result){
-        res.send('save ok')
+        res.redirect('back')
       })
     }) 
   })
 })
 app.get('/', function (req, res) {
-      Health.find({},function(err,result){
-      res.send(result)
-      })
+ res.redirect('/index.html')  
 })
+app.post('/number', function (req, res) {
+    var number = req.body.number
+    if(req.body.number/2 === 0) {
+    res.render('white.ejs',{userName: User[number]})
+    }
+    else{
+    res.render('red.ejs',{userName: User[number]})
+    }
+})
+
 setInterval(function(){
   Health.find({},function(err,result){
     io.emit('update_blood', result); 
+    console.log(result)
   })
 }, 1000);
 
@@ -56,6 +66,7 @@ io.on('connect',function(socket){
   socket.on('attack',function(msg){
     Health.update({team: msg.team},{$inc: {blood: -1}}, function(err,team){
         console.log(msg)
+        io.emit('attacker', {team: msg.myTeam, attacker: msg.userName}); 
       })
   }) 
 })
